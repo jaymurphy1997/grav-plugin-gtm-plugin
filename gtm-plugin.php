f7120bf729116c8e0e51ac42692a599533efbc45
Jay Murphy <jmurphy@trionia.com>
2024-08-06 12:38:36 -0400

    updated with multi-line code

--- gtm-plugin.php
+++ gtm-plugin.php
@@ -63,7 +63,7 @@ class GtmPluginPlugin extends Plugin
         $this->grav['assets']->addInlineJs($headCode, null, "head");
  
         $bodyCode = $this->getBodyContainerCode($containerId);
-        $content = preg_replace('"/(<body.*?>)/is", "$1".{$bodyCode}, $this->grav->output);
+        $content = preg_replace("/(<body.*?>)/is", "$1".$bodyCode, $this->grav->output);
 		$this->grav->output = $content;
     }
 
@@ -76,13 +76,9 @@ class GtmPluginPlugin extends Plugin
     {
         $code =
             "<!-- Google Tag Manager -->
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','{$gtmContainerId}');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','{$gtmContainerId}');
             <!-- End Google Tag Manager -->";
-
         return $code;
     }
 
