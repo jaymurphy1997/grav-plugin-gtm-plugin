48426dbe2b32a15afd9c2f145c4bf06856a36685
Jay Murphy <jmurphy@trionia.com>
2024-08-06 11:53:54 -0400

    updated the iframe portion of the GTM Container code

--- gtm-plugin.php
+++ gtm-plugin.php
@@ -94,8 +94,7 @@ class GtmPluginPlugin extends Plugin
     {
         $code =
             "<!-- Google Tag Manager (noscript) -->
-            <noscript><iframe src='https://www.googletagmanager.com/ns.html?id={$gtmContainerName}'
-            height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript>
+            <noscript><iframe src='https://www.googletagmanager.com/ns.html?id={$gtmContainerName}' height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript>
             <!-- End Google Tag Manager (noscript) -->";
 
         return $code;
